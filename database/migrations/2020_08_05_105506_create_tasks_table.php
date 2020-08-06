ae18fcbc0eb5f230900c25d3a5742366c0c619a1
aathiskavana <aathiskavana@github.com>
2020-08-06 20:51:09 +0700

    create database

--- database/migrations/2020_08_05_105506_create_tasks_table.php
+++ database/migrations/2020_08_05_105506_create_tasks_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CreateTasksTable extends Migration
 {
@@ -14,8 +15,11 @@ class CreateTasksTable extends Migration
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
